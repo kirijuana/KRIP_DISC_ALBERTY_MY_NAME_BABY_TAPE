8918739e1ec94afda92cd117e8b98f1a9c8cd4b7
kirijuana <sazkirand67@gmail.com>
2019-11-06 17:50:05 +0700

    Добавьте файлы проекта.

---
+++ KRIP_LAB7_ALBERTI/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KRIP_LAB7_ALBERTI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Shifr_Click(object sender, EventArgs e)
+        {
+            char[] text = Text.Text.ToCharArray();
+            char[] shifr = Text.Text.ToCharArray();
+
+            char[] ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            char[] ALPHA_copy = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            char[] alpha = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            char[] alpha_copy = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            char[] alph = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя".ToCharArray();
+            char[] alph_copy = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя".ToCharArray();
+            char[] ALPH = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ".ToCharArray();
+            char[] ALPH_copy = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ".ToCharArray();
+            int n = 0, j1 = 0, j2 = 0;      
+            int key = Convert.ToInt32(keyBox.Text);
+
+            //  сдвиг кольца
+            for (int i = 0, j = 0; i < ALPHA.Length; i++)
+            {
+
+                if(key >= 0)
+                    j = (i + key) % ALPHA.Length;
+                if (key < 0)
+                {
+                    j = (i + key + ALPHA.Length) % ALPHA.Length;
+                    while (j < 0)
+                    {
+                        j = (j + ALPHA.Length) % ALPHA.Length;
+                    }
+                }
+
+                ALPHA_copy[j] = ALPHA[i];
+                alpha_copy[j] = alpha[i];
+            }
+
+            //  сдвиг кольца
+            for (int i = 0, j = 0; i < ALPH.Length; i++)
+            {
+                if (key >= 0)
+                    j = (i + key) % ALPH.Length;
+                if (key < 0)
+                {
+                    j = (i + key + ALPH.Length) % ALPH.Length;
+                    while (j < 0)
+                    {
+                        j = (j + ALPH.Length) % ALPH.Length;
+                    }
+                }
+                ALPH_copy[j] = ALPH[i];
+                alph_copy[j] = alph[i];
+            }
+
+            for (int i = 0; i < text.Length;i++)
+            {
+                for (int j = 0; j < ALPHA.Length; j++)
+                {
+                    j1 = n + j;
+                    if (j1 >= ALPHA.Length)
+                    {
+                        j1 = j1 % 26;
+                    }
+                    if (text[i] == ALPHA[j])
+                    {
+                        shifr[i] = ALPHA_copy[j1];
+                        n++;
+                        break;
+                    }
+                    if (text[i] == alpha[j])
+                    {
+                        shifr[i] = alpha_copy[j1];
+                        n++;
+                        break;
+                    }
+                }
+
+                for (int j = 0; j < ALPH.Length; j++)
+                {
+                    j2 = n + j;
+                    if (j2 >= ALPH.Length)
+                    {
+                        j2 = j2 % 33;
+                    }
+                    if (text[i] == ALPH[j])
+                    {
+                        shifr[i] = ALPH_copy[j2];
+                        n++;
+                        break;
+                    }
+                    if (text[i] == alph[j])
+                    {
+                        shifr[i] = alph_copy[j2];
+                        n++;
+                        break;
+                    }
+                }
+
+
+            }
+
+            string text1 = new string(shifr);
+            ShifrText.Text = text1;
+
+        }
+
+        private void button_unShifr_Click(object sender, EventArgs e)
+        {
+            char[] text = Text.Text.ToCharArray();
+            char[] shifr = Text.Text.ToCharArray();
+            char[] ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            char[] ALPHA_copy = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".ToCharArray();
+            char[] alpha = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            char[] alpha_copy = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            char[] alph = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя".ToCharArray();
+            char[] alph_copy = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя".ToCharArray();
+            char[] ALPH = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ".ToCharArray();
+            char[] ALPH_copy = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ".ToCharArray();
+            int n = 0, j1 = 0, j2 = 0;
+            int key = Convert.ToInt32(keyBox.Text);
+            int length = 0;
+
+            for (int i = 0, j = 0; i < ALPHA.Length; i++)
+            {
+                if (key >= 0)
+                    j = (i + key) % ALPHA.Length;
+                if (key < 0)
+                {
+                    j = (i + key + ALPHA.Length) % ALPHA.Length;
+                    while (j < 0)
+                    {
+                        j = (j + ALPHA.Length) % ALPHA.Length;
+                    }
+                }
+                ALPHA_copy[j] = ALPHA[i];
+                alpha_copy[j] = alpha[i];
+            }
+
+            for (int i = 0, j = 0; i < ALPH.Length; i++)
+            {
+                if (key >= 0)
+                    j = (i + key) % ALPH.Length;
+                if (key < 0)
+                {
+                    j = (i + key + ALPH.Length) % ALPH.Length;
+                    while (j < 0)
+                    {
+                        j = (j + ALPH.Length) % ALPH.Length;
+                    }
+                }
+                ALPH_copy[j] = ALPH[i];
+                alph_copy[j] = alph[i];
+            }
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                for (int j = 0; j < ALPHA.Length; j++)
+                {
+                    if (text[i] == ALPHA[j] || text[i] == alpha[j])
+                    {
+                        length++;
+                        break;
+                    }
+                }
+
+                for (int j = 0; j < ALPH.Length; j++)
+                {
+                    if (text[i] == ALPH[j] || text[i] == alph[j])
+                    {
+                        length++;
+                        break;
+                    }
+                }
+            }
+
+
+            length--;
+            for (int i = text.Length - 1; i >= 0; i--)
+            {
+                for (int j = 0; j < ALPHA.Length; j++)
+                {
+                    j1 = j - length;
+
+                    while (j1 < 0)
+                    {
+                        j1 = j1 + 26;
+                    }
+
+                    if (text[i] == ALPHA_copy[j])
+                    {
+                        shifr[i] = ALPHA[j1];
+                        length--;
+                    }
+
+                    if (text[i] == alpha_copy[j])
+                    {
+                        shifr[i] = alpha[j1];
+                        length--;
+                    }
+                }
+                for (int j = 0; j < ALPH.Length; j++)
+                {
+                    j2 = j - length;
+
+                    while (j2 < 0)
+                    {
+                        j2 = j2 + 33;
+                    }
+
+                    if (text[i] == ALPH_copy[j])
+                    {
+                        shifr[i] = ALPH[j2];
+                        length--;
+                    }
+
+                    if (text[i] == alph_copy[j])
+                    {
+                        shifr[i] = alph[j2];
+                        length--;
+                    }
+                }
+            }
+
+            string text1 = new string(shifr);
+            ShifrText.Text = text1;
+        }
+    }
+}
---
+++ KRIP_LAB7_ALBERTI/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("KRIP_LAB7_ALBERTI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KRIP_LAB7_ALBERTI")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("97db2574-0fd5-40af-8f8a-b981dee0a6fe")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
